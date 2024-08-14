e25a9437e976aace00af6cb4b6109d6e04485520
baechu805 <bjoo606@naver.com>
2024-08-14 17:46:24 +0900

    fix

--- src/movdata/ml.py
+++ src/movdata/ml.py
@@ -4,7 +4,7 @@ import json
 import time
 from tqdm import tqdm
 
-API_ KEY = os.getenv('MOVIE_API_KEY')
+API_KEY = os.getenv('MOVIE_API_KEY')
 
 def save_json(data, file_path):
 	# 파일저장 경로 mkdir
@@ -16,26 +16,26 @@ def save_json(data, file_path):
 		
 def req(url):
     r = requests.get(url)
-	j = r.json()
-	return j
+    j = r.json()
+    return j
 		
 def save_movies(year, per_page=10, sleep_time=1):
-	file_path = f'data/movies/year={year}/data.json'
+    file_path = f'data/movies/year={year}/data.json'
 		
-	# 위 경로가 있으면 API 호출을 멈추고 프로그램 종료
+    # 위 경로가 있으면 API 호출을 멈추고 프로그램 종료
 	
     # totCnt 가져오고 total_pages 계산
     url_base = f"https://kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key={API_KEY}&openStartDt={year}&openEndDt={year}"
-    r = req(url_base + f"curPage=1")
+    r = req(url_base + f"&curPage=1")
     tot_cnt = r['movieListResult']['totCnt']
     total_pages = (tot_cnt // per_page) + 1
 
     # total_pages 만큼 Loop돌면서 API호출
     all_data = []
     
-    for page in range(2, total_pages + 1):
-        time.sleep(sleep_time)
-        r = req(url)
+    for page in tqdm(range(2, total_pages + 1)):
+	time.sleep(sleep_time)
+        r = req(url_base + f"&curPage={page}")
         d = r['movieListResult']['movieList']
         all_data.extend(d)
     
